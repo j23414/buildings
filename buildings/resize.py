02f4f362f2d08da52a320fc7836f80ebb36c4ec9
j23414 <jenchang@iastate.edu>
2022-03-18 11:54:06 -0500

    refactor and try mypy

--- buildings/resize.py
+++ buildings/resize.py
@@ -3,10 +3,9 @@
 # (1) Import Libraries
 import sys
 import argparse
-import pandas as pd   # tab delimited files
-from PIL import Image
 import os
 from glob import glob
+from PIL import Image
 
 # (2) Define command line arguments
 def parse_args():
@@ -24,19 +23,25 @@ def parse_args():
     return parser.parse_args()
 
 # (3) Reusable functions
-def resize_800width(imgfile):
-  i = Image.open(imgfile)
-  if i.mode != 'RGB':
-    i = i.convert('RGB')
-  width, height = i.size
-  if width > height:
-    i.save("resized/" + os.path.basename(imgfile))  # Ignore landscapped items
-    return
-  ratio = height / width
-  new_width = 800
-  new_height = int(ratio * new_width)
-  new_i = i.resize((new_width, new_height))
-  new_i.save("resized/" + os.path.basename(imgfile))
+def resize_800width(imgfile:str, outdir:str = 'resized', new_width:int = 800):
+  img = Image.open(imgfile)
+  if img.mode != 'RGB':
+    img = img.convert('RGB')
+  width, height = img.size
+  
+  if width > height: # Ignore landscaped items
+    new_img = img
+  elif width < 800:  # Ignore smaller images
+    new_img = img
+  else:              # Resize image
+    ratio = height / width
+    new_height = int(ratio * new_width)
+    new_img = img.resize((new_width, new_height))
+
+  # Save image to new folder
+  if not os.path.exists(outdir):
+    os.makedirs(outdir)
+  new_img.save(outdir + '/' + os.path.basename(imgfile))
 
 # (4) Main call, connecting arguments to reusable functions (workflow)
 def main():
